c4592aa16859b56827a06cd2f47ab7ba702767b3
liu-liuhai <769891117@qq.com>
2024-05-18 12:20:52 +0800

    添加项目文件。

---
+++ interfaceTest/InterfaceReview.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceReview
+{
+    interface IElectrcial
+    {
+        string ProductionDate { get; set; }
+        void Run();
+    }
+    public class TV : IElectrcial
+    {
+        public double Price { get; set; }
+        public string Brand { get; set; }
+        public TV(double price, string brand, string ProductionDate)
+        {
+            this.Brand = brand;
+            this.ProductionDate = ProductionDate;
+            this.Price = price;
+        }
+
+        private string _ProductionDate = "";
+        public string ProductionDate
+        {
+            get { return _ProductionDate; }
+            set { _ProductionDate = value; }
+        }
+        public void Run()
+        {
+            Console.WriteLine("The TV is Running");
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Brand： " + Brand);
+            Console.WriteLine("Price： " + Price);
+            Console.WriteLine("ProductionDate： " + ProductionDate);
+        }
+    }
+}
---
+++ interfaceTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using InterfaceReview;
+namespace interfaceTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TV tv = new TV(8000.00,"HUAWEI","2024-1-1");
+            tv.Run();
+            tv.ShowInfo();
+        }
+    }
+    
+}
